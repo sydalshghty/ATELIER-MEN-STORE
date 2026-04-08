54348783adf621c8a1d4655b886afeb999e96c3d
Saeed Mohamed <sydalshghty15@gmail.com>
2026-04-08 10:49:07 +0200

    update-changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/',
   plugins: [react(), tailwindcss()],
-  base: '/ATELIER-MEN-STORE/',
 })
