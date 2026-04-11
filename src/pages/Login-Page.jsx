7727b15f5a7476b745bc9dc3a62fbc5fc64ab013
Saeed Mohamed <sydalshghty15@gmail.com>
2026-04-11 14:32:36 +0200

    upload-new-changes

--- src/pages/Login-Page.jsx
+++ src/pages/Login-Page.jsx
@@ -16,20 +16,20 @@ function LoginPage() {
                     <label className="text-[14px] text-(--color-label) font-medium uppercase">Email Address</label>
                     <input type="email" placeholder="name@example.com" name="email" className="w-full h-14.5 p-4 border border-(--color-border) rounded-sm outline-none text-[16px] text-(--color-label) bg-white font-medium placeholder:pl-1" />
                 </div>
-                <div className="col-password">
-                    <div>
-                        <label>Password</label>
-                        <a href="">Forgot?</a>
+                <div className="col-password flex flex-col gap-2 mb-6 w-full">
+                    <div className="flex justify-between items-center">
+                        <label className="text-[14px] text-(--color-label) font-medium uppercase">Password</label>
+                        <a href="#" className="text-(--color-forget) text-[14px] font-bold">Forgot?</a>
                     </div>
-                    <input type="password" name="password" />
+                    <input type="password" name="password" className="w-full h-14.5 p-4 border border-(--color-border) rounded-sm outline-none text-[16px] text-(--color-label) bg-white font-medium placeholder:pl-1" />
                 </div>
-                <div className="col-check">
-                    <span>
-                        <Check />
+                <div className="col-check flex items-center gap-2 mb-6" >
+                    <span className="w-4 h-4 border border-(--color-border) rounded-xs flex justify-center items-center cursor-pointer">
+                        <Check className="text-white" />
                     </span>
-                    <p>Stay signed in for 30 days</p>
+                    <p className="text-[14px] text-(--color-text)">Stay signed in for 30 days</p>
                 </div>
-                <input type="submit" value={`SIGN IN`} />
+                <input type="submit" value={`SIGN IN`} className="mb-6 cursor-pointer w-full h-15 bg-(--color-forget) rounded-sm text-[16px] text-white" />
             </form>
             <span className="continue-with">Or continue with</span>
             <div className="google-facebook-btns">
